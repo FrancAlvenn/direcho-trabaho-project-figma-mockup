bc4cddd01f707a15390ecb14ab3ebd98186d02ed
mrkmartin <martinmarkpaulo12@gmail.com>
2025-05-30 18:32:49 +0800

    Add about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import EventDetailPage from "./components/EventDetail/EventDetailPage";
 
 function App() {
   return (
     <div className="App">
-      <div className="w-48 h-48 rounded-lg bg-blue-500"></div>
+      <EventDetailPage />
     </div>
   );
 }
