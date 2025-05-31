17f3faea4583cf1f99660e51572ec31f55d34da3
Rommel Dones <104125810+omeldon@users.noreply.github.com>
2025-05-31 06:19:50 +0000

    Get Card_image on events.json

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,7 @@ function App() {
       <HomePage/>
       {/* <EventViewPage/> */}
       {/* <EventDetailPage /> */}
-      {/* <AboutSection/>
-      <EventsContainer /> */}
+      {/* <AboutSection/> */}
     </div>
   );
 }
--- src/components/AboutPage/About.jsx
+++ src/components/AboutPage/About.jsx
@@ -1,14 +1,31 @@
-import bgImage from "../../assets/images/bom-2.png";
+import { useEffect, useState } from "react";
+import { getEvents } from "../../services/events.service";
 import UsefulInfoSP from "./UsefulInfoSP";
 import PriceRange from "./PriceRange";
 import AboutSection from "./AboutSection";
 
 const About = () => {
+  const [event, setEvent] = useState(null);
+
+  useEffect(() => {
+    getEvents()
+      .then((data) => {
+        setEvent(data[0]);
+      })
+      .catch((err) => {
+        console.error("Error:", err);
+      });
+  }, []);
+
+  if (!event) {
+    return <div className="text-white p-6">Loading...</div>;
+  }
+
   return (
     <div className="flex bg-black w-full h-[1440px] min-h-screen mx-auto font-sans">
       <div
         className="events-container flex flex-wrap justify-between pb-10 pt-10 bg-black bg-cover bg-center w-full h-[600px]"
-        style={{ backgroundImage: `url(${bgImage})` }}
+        style={{ backgroundImage: `url(${event.card_image})` }}
       >
         <div className="flex flex-col w-full lg:w-2/3 px-4 sm:px-8 md:px-[120px] mt-[16px] sm:mt-[8%] md:mt-[10%] lg:mt-[12%]">
           <AboutSection />
@@ -23,4 +40,3 @@ const About = () => {
 };
 
 export default About;
-
