0950375961f15e3442713d478b8e344d2d4881fc
Franc Alvenn Dela Cruz <francalvenndelacruz@gmail.com>
2025-05-31 10:57:47 +0800

    Add React Router for navigation and update EventCard to handle routing

--- src/pages/EventViewPage.jsx
+++ src/pages/EventViewPage.jsx
@@ -1,13 +1,16 @@
+import { useParams } from "react-router";
 import AboutSection from "../components/AboutPage/About";
 import EventsContainer from "../components/EventsCard/EventsContainer";
 import Navbar from "../components/Navbar";
 
 
 const EventViewPage = () => {
+  const eventId = useParams().event_id;
+
   return (
     <div className="bg-black pb-10 pt-10">
         <Navbar />
-        <AboutSection />
+        <AboutSection/>
         <EventsContainer title="More like this"/>
     </div>
   );
